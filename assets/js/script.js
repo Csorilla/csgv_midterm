b0eddf09664f242fdb883ebf8432a2f59cbc7041
Csorilla <cristinejoy.sorilla@csav.edu.ph>
2025-04-02 10:28:03 +0800

    Update index.html for readability Update index.html for readability

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
+function closeOffcanvas() {
+    // Close the offcanvas menu
+    var offcanvasElement = document.getElementById("mobileMenu");
+    var bsOffcanvas = bootstrap.Offcanvas.getInstance(offcanvasElement);
+    if (bsOffcanvas) {
+      bsOffcanvas.hide();
+    }
+  }
 
+  function scrollToDonation() {
+    closeOffcanvas(); // Close the menu first
+
+    setTimeout(() => {
+      document
+        .getElementById("donation-section")
+        .scrollIntoView({ behavior: "smooth" });
+    }, 320); // Delay to ensure transition
+    
+}
